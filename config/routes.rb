2c2ce77bb774ac31a4f8c24b96001e9613c4eec4
Dae <juliandaeone@gmail.com>
2018-05-09 18:04:57 +1000

    change navbar links

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :charges
   root 'welcome#index'
-
+  get 'home', to: 'welcome#index'
   require 'carrierwave/orm/activerecord'
 end
