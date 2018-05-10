d07cf1f39a9aabcd4fe5f92262d909ec6a7751d0
Dae <juliandaeone@gmail.com>
2018-05-10 10:27:15 +1000

    fix navbar link to home

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :charges
-  root 'welcome#index'
-  get 'home', to: 'welcome#index'
+  root 'pages#home'
+
+  # get 'home', to: 'pages#home'
   require 'carrierwave/orm/activerecord'
 end
